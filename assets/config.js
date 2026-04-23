1ae7bb6ff656ccde93685b950ecafc020101a55b
stacyliao0206 <stacy@numeracylab.com>
2026-04-23 16:57:28 +0800

    Add assets folder

---
+++ assets/config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  useLiveData: false,
+
+  sheets: {
+    skus: "",
+    transactions: "",
+    warehouses: "",
+    audits: ""
+  },
+
+  camps: [
+    { name: "2026 冬令營", date: "2026-02-10", students: 1500 },
+    { name: "2026 夏令營", date: "2026-07-15", students: 4000 },
+    { name: "2027 冬令營", date: "2027-02-10", students: 1500 },
+    { name: "2027 夏令營", date: "2027-07-15", students: 4000 }
+  ],
+
+  staleMonths: 12
+};
