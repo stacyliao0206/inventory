828370f68baa9c5057915f470ba9528b67b84ee5
stacyliao0206 <stacy@numeracylab.com>
2026-05-12 10:46:43 +0800

    Update Sheets URLs to v4

--- assets/config.js
+++ assets/config.js
@@ -2,11 +2,16 @@ const CONFIG = {
   useLiveData: true,
 
   sheets: {
-  skus:         "https://docs.google.com/spreadsheets/d/e/2PACX-1vThN0RKLSU67oXkUFIixgnX73N8mPiiEchEGnkpeIpvr6K6WUsnOzubA1hwiPFc4A/pub?gid=1586570316&single=true&output=csv",
-  transactions: "https://docs.google.com/spreadsheets/d/e/2PACX-1vThN0RKLSU67oXkUFIixgnX73N8mPiiEchEGnkpeIpvr6K6WUsnOzubA1hwiPFc4A/pub?gid=1250416564&single=true&output=csv",
-  warehouses:   "https://docs.google.com/spreadsheets/d/e/2PACX-1vThN0RKLSU67oXkUFIixgnX73N8mPiiEchEGnkpeIpvr6K6WUsnOzubA1hwiPFc4A/pub?gid=1345876107&single=true&output=csv",
-  audits:       "https://docs.google.com/spreadsheets/d/e/2PACX-1vThN0RKLSU67oXkUFIixgnX73N8mPiiEchEGnkpeIpvr6K6WUsnOzubA1hwiPFc4A/pub?gid=596060522&single=true&output=csv",
-  bom:          "https://docs.google.com/spreadsheets/d/e/2PACX-1vThN0RKLSU67oXkUFIixgnX73N8mPiiEchEGnkpeIpvr6K6WUsnOzubA1hwiPFc4A/pub?gid=406889349&single=true&output=csv"
+  skus:
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vTGk2jmOG_sgx8_U-YcEozxWKvk3Ak9Bj7TQfJpZDZcnU8g2D7TkVP8uXqBW4Gl1A/pub?gid=387231423&single=true&output=csv",
+  transactions: 
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vTGk2jmOG_sgx8_U-YcEozxWKvk3Ak9Bj7TQfJpZDZcnU8g2D7TkVP8uXqBW4Gl1A/pub?gid=575030737&single=true&output=csv",
+  warehouses:
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vTGk2jmOG_sgx8_U-YcEozxWKvk3Ak9Bj7TQfJpZDZcnU8g2D7TkVP8uXqBW4Gl1A/pub?gid=1997329772&single=true&output=csv",
+  audits:
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vTGk2jmOG_sgx8_U-YcEozxWKvk3Ak9Bj7TQfJpZDZcnU8g2D7TkVP8uXqBW4Gl1A/pub?gid=152065121&single=true&output=csv",
+  bom:
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vTGk2jmOG_sgx8_U-YcEozxWKvk3Ak9Bj7TQfJpZDZcnU8g2D7TkVP8uXqBW4Gl1A/pub?gid=855118257&single=true&output=csv"
   },
 
   camps: [
